82e30f259ff7f4593b30927e45615531c3dd803f
Egor Ukolov <ukolovegor@yandex.ru>
2021-07-06 23:22:32 +0700

    Initial Commit

---
+++ HelloWorldApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorldApp
+//
+//  Created by Egor Ukolov on 06.07.2021.
+//  Copyright © 2021 Egor Ukolov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
